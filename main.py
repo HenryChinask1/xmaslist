c8f1541c5205b1ab97ee98d35335e8e4d6b2b143
HenryChinask1 <jeffbaier72@yahoo.com>
2025-11-26 00:29:14 -0500

    A little polishing

--- main.py
+++ main.py
@@ -4,27 +4,29 @@ import random, config
 
 class Name:
 
-    def __init__(self, name, email, conflicts, number):
+    def __init__(self, name, email, conflicts):
         self.name = name
         self.email = email
         self.conflicts = conflicts
-        self.number = number
         self.pick = ''
 
-beth = Name('Beth', 'bethbaier1007@outlook.com', ['Jeff', 'Beth'], 0)
-jeff = Name('Jeff', 'jeffbaier72@yahoo.com', ['Beth', 'Jeff'], 1)
-jude = Name('Jude', 'judeherman1@aol.com', ['Greg', 'Jude'], 2)
-greg = Name('Greg', 'undecidedgd@yahoo.com', ['Jude', 'Greg'], 3)
-joel = Name('Joel', 'jbaier@ewmi.com', ['Chelsea', 'Joel'], 4)
-chelsea = Name('Chelsea', 'ex@ex.com',['Joel', 'Chelsea'], 5)
-jerry = Name('Jerry', 'ex@ex.com', ['Cindy', 'Jerry'], 6)
-cindy = Name('Cindy', 'ex@ex.com', ['Jerry', 'Cindy'], 7)
+# Load in the gang.
+beth = Name('Beth', 'bethbaier1007@outlook.com', ['Jeff', 'Beth'])
+jeff = Name('Jeff', 'jeffbaier72@yahoo.com', ['Beth', 'Jeff'])
+jude = Name('Jude', 'judeherman1@aol.com', ['Greg', 'Jude'])
+greg = Name('Greg', 'undecidedgd@yahoo.com', ['Jude', 'Greg'])
+joel = Name('Joel', 'jbaier@ewmi.com', ['Chelsea', 'Joel'])
+chelsea = Name('Chelsea', 'ex@ex.com',['Joel', 'Chelsea'])
+jerry = Name('Jerry', 'ex@ex.com', ['Cindy', 'Jerry'])
+cindy = Name('Cindy', 'ex@ex.com', ['Jerry', 'Cindy'])
 
+# Add everyone to two lists and then mix 'em up.
 names = [beth, jeff, jude, greg, joel, chelsea, jerry, cindy]
 random.shuffle(names)
 picks = [beth, jeff, jude, greg, joel, chelsea, jerry, cindy]
 random.shuffle(picks)
 
+# Choose names until there is a conflict, then keep starting over.
 i = 0
 while picks and i < 8:
     if picks[-1].name not in names[i].conflicts:
@@ -38,16 +40,7 @@ while picks and i < 8:
         random.shuffle(picks)
         i = 0
 
-for i in names:
-    print(i.name, i.pick)
-
-
-subject = 'Your Secret Santa'
-body = f'Your secret santa is {jeff.pick}!'
-sender = config.EMAILUSER
-recipients = jeff.email
-password = config.EMAILPASSWORD
-
+# The email sender.
 def send_email(subject, body, sender, recipients, password):
     msg = MIMEText(body)
     msg['Subject'] = subject
@@ -58,4 +51,11 @@ def send_email(subject, body, sender, recipients, password):
         smtp_server.sendmail(sender, recipients, msg.as_string())
     print('Message Sent')
 
-send_email(subject, body, sender, recipients, password)
+# Fire out the emails.
+for i in names:
+    subject = 'Your Secret Santa'
+    body = f'Your secret santa is {i.pick}!'
+    sender = config.EMAILUSER
+    password = config.EMAILPASSWORD
+    recipients = i.email
+    send_email(subject, body, sender, recipients, password)
