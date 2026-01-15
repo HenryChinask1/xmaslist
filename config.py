49d51017cc8e42b9d8a5035eaeac9597d6539f31
HenryChinask1 <jeffbaier72@yahoo.com>
2026-01-14 23:53:37 -0500

    put this in a venv and added it to gitignore.

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import smtplib
 from email.mime.text import MIMEText
 
 # Email info.
-f = open('info.txt').read().split('\n')
-EMAILUSER = f[0]
-EMAILPASSWORD = f[1]
-abbeyEmail = f[2]
-bethEmail = f[3] 
-jeffEmail = f[4]
-judeEmail = f[5]
-gregEmail = f[6]
-joelEmail = f[7]
-chelseaEmail = f[8]
-jerryEmail = f[9]
-cindyEmail = f[10]
-nateEmail = f[11]
+with open('info.txt').read().split('\n') as f:
+    EMAILUSER = f[0]
+    EMAILPASSWORD = f[1]
+    abbeyEmail = f[2]
+    bethEmail = f[3] 
+    jeffEmail = f[4]
+    judeEmail = f[5]
+    gregEmail = f[6]
+    joelEmail = f[7]
+    chelseaEmail = f[8]
+    jerryEmail = f[9]
+    cindyEmail = f[10]
+    nateEmail = f[11]
 
 # The people as objects.
 class Name:
