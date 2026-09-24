9d21590ef7b54f7141686c2fda6917e5ca939515
henryChinask1 <jeffbaier72@yahoo.com>
2026-09-23 23:52:32 -0400

    tightened this into one file and made the secret stuff a py file insead

--- xmaslist/main.py
+++ xmaslist/main.py
@@ -1,19 +1,37 @@
-import random, config
-from config import Name, send_email
+import random, smtplib
+from emails import sender_email, emails
+from email.mime.text import MIMEText
+
+class Name:
+    def __init__(self, name: str, email: str, conflicts: list[str]):
+        self.name: str = name
+        self.email: str = email
+        self.conflicts: list[str] = conflicts
+        self.pick: str = ''
+
+def send_email(subject: str, body: str, sender: str, recipients: str, password: str):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipients
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
+    print('Message Sent')
 
 def main():
     
     # Load in the gang.
-    beth: Name = Name('Beth', config.bethEmail, ['Jeff', 'Beth'])
-    jeff: Name = Name('Jeff', config.jeffEmail, ['Beth', 'Jeff'])
-    jude: Name = Name('Jude', config.judeEmail, ['Greg', 'Jude'])
-    greg: Name = Name('Greg', config.gregEmail, ['Jude', 'Greg'])
-    joel: Name = Name('Joel', config.joelEmail, ['Chelsea', 'Joel'])
-    chelsea: Name = Name('Chelsea', config.chelseaEmail,['Joel', 'Chelsea'])
-    jerry: Name = Name('Jerry', config.jerryEmail, ['Cindy', 'Jerry'])
-    cindy: Name = Name('Cindy', config.cindyEmail, ['Jerry', 'Cindy'])
-    nate: Name = Name('Nate', config.nateEmail, ['Nate'])
-    abbey: Name = Name('Abbey', config.abbeyEmail, ['Abbey'])
+    beth: Name = Name('Beth', emails['beth'], ['Jeff', 'Beth'])
+    jeff: Name = Name('Jeff', emails['jeff'], ['Beth', 'Jeff'])
+    jude: Name = Name('Jude', emails['jude'], ['Greg', 'Jude'])
+    greg: Name = Name('Greg', emails['greg'], ['Jude', 'Greg'])
+    joel: Name = Name('Joel', emails['joel'], ['Chelsea', 'Joel'])
+    chelsea: Name = Name('Chelsea', emails['chelsea'],['Joel', 'Chelsea'])
+    jerry: Name = Name('Jerry', emails['jerry'], ['Cindy', 'Jerry'])
+    cindy: Name = Name('Cindy', emails['cindy'], ['Jerry', 'Cindy'])
+    nate: Name = Name('Nate', emails['nate'], ['Nate'])
+    abbey: Name = Name('Abbey', emails['abbey'], ['Abbey'])
 
     # Add everyone to two lists and then mix 'em up.
     names: list[Name] = [beth, jeff, jude, greg, joel, chelsea, jerry, cindy, nate]
@@ -44,15 +62,15 @@ def main():
         subject: str = 'Your Secret Santa'
         body: str = f'Your secret santa is {recipient.pick}!\n\n It took {trys} repicks to get it right.'
         allPicks.append(f'{recipient.name} picked {recipient.pick}')
-        sender: str = config.EMAILUSER
-        password: str = config.EMAILPASSWORD
+        sender: str = sender_email['email']
+        password: str = sender_email['key']
         recipients: str = recipient.email
         if recipient.email:
             send_email(subject, body, sender, recipients, password)
 
     # Send the all picks list to Abbey to check.
     allBody: str = f'The secret santa picks are {allPicks}.\n\nIt took {trys} repicks to get it right.'
-    send_email("All secret santa", allBody, config.EMAILUSER, abbey.email, config.EMAILPASSWORD)
+    send_email("All secret santa", allBody, sender_email['email'], abbey.email, sender_email['password'])
 
 if __name__ == '__main__':
     main()
