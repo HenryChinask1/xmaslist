95d1736b0d0febc9e3bb99c206431472ab7afd91
henryChinask1 <jeffbaier72@yahoo.com>
2026-09-24 10:02:10 -0400

    made the whole script a little more generic one file to put emails in and the whole thing runs from main

--- xmaslist/main.py
+++ xmaslist/main.py
@@ -1,5 +1,5 @@
 import random, smtplib
-from emails import sender_email, emails
+from emails import sender_email, santas, secret_keeper
 from email.mime.text import MIMEText
 
 class Name:
@@ -8,6 +8,21 @@ class Name:
         self.email: str = email
         self.conflicts: list[str] = conflicts
         self.pick: str = ''
+
+def create_names() -> tuple[list[Name], list[Name]]:
+    names: list[Name] = []
+    picks: list[Name] = []
+
+    # Load in your gang from emails.py
+    # emails should be in the form {name: (email, [exclusions])}
+    for name, email in santas.items():
+        name = Name(name, email[0], email[1])
+        names.append(name)
+        picks.append(name)
+
+    random.shuffle(names)
+    random.shuffle(picks)
+    return names, picks
 
 def send_email(subject: str, body: str, sender: str, recipients: str, password: str):
     msg = MIMEText(body)
@@ -20,38 +35,18 @@ def send_email(subject: str, body: str, sender: str, recipients: str, password: 
     print('Message Sent')
 
 def main():
-    
-    # Load in the gang.
-    beth: Name = Name('Beth', emails['beth'], ['Jeff', 'Beth'])
-    jeff: Name = Name('Jeff', emails['jeff'], ['Beth', 'Jeff'])
-    jude: Name = Name('Jude', emails['jude'], ['Greg', 'Jude'])
-    greg: Name = Name('Greg', emails['greg'], ['Jude', 'Greg'])
-    joel: Name = Name('Joel', emails['joel'], ['Chelsea', 'Joel'])
-    chelsea: Name = Name('Chelsea', emails['chelsea'],['Joel', 'Chelsea'])
-    jerry: Name = Name('Jerry', emails['jerry'], ['Cindy', 'Jerry'])
-    cindy: Name = Name('Cindy', emails['cindy'], ['Jerry', 'Cindy'])
-    nate: Name = Name('Nate', emails['nate'], ['Nate'])
-    abbey: Name = Name('Abbey', emails['abbey'], ['Abbey'])
 
-    # Add everyone to two lists and then mix 'em up.
-    names: list[Name] = [beth, jeff, jude, greg, joel, chelsea, jerry, cindy, nate]
-    random.shuffle(names)
-    picks: list[Name] = [beth, jeff, jude, greg, joel, chelsea, jerry, cindy, nate]
-    random.shuffle(picks)
-
+    (names, picks) = create_names()
     # Choose names until there is a conflict, then keep starting over.
     picked: int = 0
     trys: int = 0
-    while picks and picked < 9:
+    while picks and picked < len(names):
         if picks[-1].name not in names[picked].conflicts:
             names[picked].pick = picks[-1].name
             picks.pop()
             picked += 1
         else: # Start over.
-            picks: list[Name] = [beth, jeff, jude, greg, joel, chelsea, jerry, cindy, nate]
-            for name_picking in names:
-                name_picking.pick = ''
-            random.shuffle(picks)
+            (names, picks) = create_names()
             picked = 0
             trys += 1
 
@@ -65,12 +60,12 @@ def main():
         sender: str = sender_email['email']
         password: str = sender_email['key']
         recipients: str = recipient.email
-        if recipient.email:
-            send_email(subject, body, sender, recipients, password)
+        send_email(subject, body, sender, recipients, password)
 
-    # Send the all picks list to Abbey to check.
+    # Send the all picks list to the secret_keeper to check.
     allBody: str = f'The secret santa picks are {allPicks}.\n\nIt took {trys} repicks to get it right.'
-    send_email("All secret santa", allBody, sender_email['email'], abbey.email, sender_email['password'])
+    knows_all: Name = Name(secret_keeper[0], secret_keeper[1], [secret_keeper[0]])
+    send_email("All secret santa picks", allBody, sender_email['email'], knows_all.email, sender_email['password'])
 
 if __name__ == '__main__':
     main()
